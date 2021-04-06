8d626735032ff7b4bab0428d6f33266559d70080
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-04-06 21:31:21 +0300

    edited datefield to be more modern and edited requirements file

--- interNect/intern/forms.py
+++ interNect/intern/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import  StringField,PasswordField,SubmitField,BooleanField,DateField
+from wtforms import  StringField,PasswordField,SubmitField,BooleanField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired,EqualTo,Length,Email,email_validator,ValidationError
 
 class InternRegistrationForm(FlaskForm):
@@ -11,7 +12,7 @@ class InternRegistrationForm(FlaskForm):
                                             Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     gender = StringField('Gender', validators=[DataRequired()])
-    date_of_birth = DateField('DateOfBirth', format='%D/%m/%Y')
+    date_of_birth = DateField('DateOfBirth', format='%Y-%m-%D')
     phone_number = StringField('Phone Number', validators=[DataRequired()])
     description = StringField('Description (Optional)')
     password = PasswordField('Password', validators=[DataRequired()])
