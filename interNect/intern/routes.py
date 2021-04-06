6f7de2aed46ad4d65152fb12a7438e8f3155a2ab
abdi-edoc-de <abbdrer464@gmail.com>
2021-04-06 21:47:21 +0300

    add register route to intern route

--- interNect/intern/routes.py
+++ interNect/intern/routes.py
@@ -1,6 +1,6 @@
 from flask import (Blueprint, render_template,url_for,
                     flash,redirect,request,abort)
-
+from InterNect.intern.forms import InternRegistrationForm
 
 from interNect import bcrypt,db
 
@@ -13,6 +13,17 @@ from interNect import bcrypt,db
 
 
 intern=Blueprint('intern',__name__)
+
+@intern.route('/register')
+def refgister():
+    form=InternRegistrationForm()
+    if form.validate_on_submit():
+        print()
+        print()
+        print(form.first_name.data)
+        print()
+        print()
+    return render_template('intern_register.html',form=form)
 
 
 
