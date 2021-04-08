962f247f3ccbc734c85646d5269f3ac30fb64bf3
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-04-08 06:44:42 +0300

    Made description textarea field

--- interNect/intern/forms.py
+++ interNect/intern/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import  StringField,PasswordField,SubmitField,BooleanField,SelectField
+from wtforms import  StringField,PasswordField,SubmitField,BooleanField,SelectField,TextAreaField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired,EqualTo,Length,Email,email_validator,ValidationError
 
@@ -12,9 +12,9 @@ class InternRegistrationForm(FlaskForm):
                                             Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     gender = SelectField('Gender',  choices = [('M', 'Male'), ('F', 'Female')], validators=[DataRequired()])
-    date_of_birth = DateField('Date of Birth', format='%Y-%m-%D')
+    date_of_birth = DateField('Date of Birth', format='%m-%D-%Y')
     phone_number = StringField('Phone Number', validators=[DataRequired()])
-    description = StringField('Description (Optional)')
+    description = TextAreaField('Description (Optional)')
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', 
                             validators=[DataRequired(),
