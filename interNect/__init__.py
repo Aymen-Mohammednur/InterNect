0fd4d31802d4a5576eed7ff1f3140142af87fab6
abdi-edoc-de <abbdrer464@gmail.com>
2021-04-08 13:52:13 +0300

    changeing the page

--- interNect/__init__.py
+++ interNect/__init__.py
@@ -11,9 +11,9 @@ from interNect.temp import create
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-# login_manager = LoginManager()
-# login_manager.login_view='users.login' 
-# login_manager.login_message_category="info"
+login_manager = LoginManager()
+login_manager.login_view='main.login' 
+login_manager.login_message_category="info"
 
 mail=Mail()
 
@@ -30,7 +30,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     
 
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
 
     from interNect.intern.routes import intern
--- interNect/intern/routes.py
+++ interNect/intern/routes.py
@@ -1,7 +1,6 @@
 from flask import (Blueprint, render_template,url_for,
                     flash,redirect,request,abort)
 from interNect.intern.forms import InternRegistrationForm
-from 
 
 from interNect import bcrypt,db
 
